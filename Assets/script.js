dd5bd636c91792b87d755aca16b9756049f00d1d
jayasreeyuvi <jayasreeragou@gmail.com>
2023-04-10 21:09:44 -0300

    inital update

--- Assets/script.js
+++ Assets/script.js
@@ -13,5 +13,40 @@ function initPage() {
     var fivedayEl = document.getElementById("days-header");
     var todayweatherEl = document.getElementById("today-weather");
     let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+
+    //unique API to a variable//
+    const APIKey = "9abea5dfd63f94e60ee99e562690a01d";
+
+    function getWeather(cityName) {
+
+        // current weather get request from open weather api//
+        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+        // Axios is a popular JavaScript library used to make HTTP requests.//
+        //It is a promise-based library that is easy to use and provides an elegant way to handle errors. //
+        //Axios can be used in both the front-end and back-end of web applications.//
+        axios.get(queryURL)
+
+        .then(function (response) {
+
+            todayweatherEl.classList.remove("d-none");
+            
+            // response to display current weather//
+            const currentDate = new Date(response.data.dt * 1000);
+            const day = currentDate.getDate();
+            const month = currentDate.getMonth() + 1;
+            const year = currentDate.getFullYear();
+            nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+            let weatherPic = response.data.weather[0].icon;
+            currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+            currentPicEl.setAttribute("alt", response.data.weather[0].description);
+
+        })
+    }
+
+
+
+
+
+
 }
 initPage();
